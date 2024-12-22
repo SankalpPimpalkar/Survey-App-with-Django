0110cea2776e3b85548f7c9e1156cf8fa56f4ab9
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-23 02:06:14 +0530

    tailwind configurations issue [BUG FIX 9]

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-echo "Python version:"
-python3 --version
+# Install dependencies
+pip install -r requirements.txt
 
-echo "Installing dependencies..."
-pip3 install -r requirements.txt
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+apt-get install -y nodejs
 
+# Debugging: Check Node.js and npm versions
+node --version
+npm --version
+
+# Set up Django
 python3 manage.py collectstatic --noinput
 python3 manage.py tailwind build
+python3 manage.py migrate
