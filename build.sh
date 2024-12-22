2662318c3caede1da4f9d5d16c2c8707bc3aca5b
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-23 01:56:33 +0530

    tailwind configurations issue [BUG FIX 6]

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 echo "Python version:"
 python3 --version
 
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
 python3 manage.py collectstatic --noinput
 python3 manage.py tailwind build
