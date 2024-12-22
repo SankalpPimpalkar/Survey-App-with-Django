25a991a653bf0a79b8f9e36aa07d1fd5cc6b12bc
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-23 02:31:53 +0530

    tailwind configurations issue [BUG FIX 14]

--- survey/settings.py
+++ survey/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/theme/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
