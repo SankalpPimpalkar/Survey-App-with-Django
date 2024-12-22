45ea1ea1596456ac820551188edab69bb8a56041
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-23 01:38:55 +0530

    tailwind configurations issue [BUG FIX 2]

--- survey/settings.py
+++ survey/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'theme/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -142,3 +142,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TAILWIND_APP_NAME = 'theme'
 NPM_BIN_PATH = "/usr/bin/npm"
+TAILWIND_CSS_PATH = 'css/dist/styles.css'
