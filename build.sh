a04c511301dc9ec594f7eae733746e8e67991e97
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-23 01:54:26 +0530

    tailwind configurations issue [BUG FIX 5]

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python manage.py collectstatic --noinput
-python manage.py tailwind build
+echo "Python version:"
+python3 --version
+
+python3 manage.py collectstatic --noinput
+python3 manage.py tailwind build
